f4052f2495f58760ebe335ca0d197270cff92e42
Geoffroy Lesage <gefthefrench@gmail.com>
2015-06-04 12:58:16 -0500

    Fixedup relations has_and_belongs_to_many

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,8 +2,7 @@ class Room < ActiveRecord::Base
 	validates :name, presence: true
 	validates :token, presence: true
 
-	belongs_to :member, as: :owner, dependent: :destroy
-	has_many :members, dependent: :destroy
+	has_and_belongs_to_many :members, dependent: :destroy
 	has_many :air_squads, dependent: :destroy
 	has_many :ground_squads, dependent: :destroy
 	has_one :match, dependent: :destroy
